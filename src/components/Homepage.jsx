fe1493dbd3ea6faadbea9f2201d216e70a539c52
Brad Herr <herrbrad@yahoo.com>
2022-02-09 13:18:05 -0500

    more data fetching

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -27,6 +27,10 @@ const Homepage = () => {
         <Col span={12}><Statistic title='Total 24h Volume' value={millify(globalStats.total24hVolume)} /></Col>
         <Col span={12}><Statistic title='Total Markets' value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the World</Title>
+        <Title level={3} className='show-more'><Link to='/cryptocurrencies'>Show More</Link></Title>
+      </div>
   </>
   );
 };
